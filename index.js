57de15d942ef452958d131eda5bc7d85e848222e
Alex <alex.gabriel@mail.ru>
2020-03-11 23:32:59 +0300

    feat: add picture grid interactivity

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const NAV = document.getElementById('nav');
-
-const navLis = NAV.querySelectorAll('li');
 const navLisA = NAV.querySelectorAll('li > a');
+const PICTUREGRID = document.getElementById('pictures');
+const pictures = PICTUREGRID.querySelectorAll('img');
 
 NAV.addEventListener('click', event => {
   navLisA.forEach(a => a.classList.remove('active-menu'));
   event.target.classList.add('active-menu');
+});
+
+PICTUREGRID.addEventListener('click', event => {
+  console.log(event);
+  pictures.forEach(img => img.classList.remove('active-picture'));
+  if (event.target.tagName === 'IMG')
+    event.target.classList.add('active-picture');
 });
