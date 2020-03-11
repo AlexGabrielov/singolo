1f2249e9e689e9d5d3a7ac51e832b30c42896ce4
Alex <alex.gabriel@mail.ru>
2020-03-11 21:19:28 +0300

    feat: finish active header

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// alert('kus');
+const NAV = document.getElementById('nav');
+
+const navLis = NAV.querySelectorAll('li');
+const navLisA = NAV.querySelectorAll('li > a');
+
+NAV.addEventListener('click', event => {
+  navLisA.forEach(a => a.classList.remove('active-menu'));
+  event.target.classList.add('active-menu');
+});
