f747f47ec9265636d55af124b7b0c7dad427a00c
Alex <alex.gabriel@mail.ru>
2020-03-12 14:15:08 +0300

    feat: add form interaction

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const sliderPictures = SLIDER.querySelectorAll('img');
 const FILTER = document.getElementById('filter');
 const filterButtons = FILTER.querySelectorAll('li > button');
 const filterSpans = FILTER.querySelectorAll('li > button > span');
+const MESSAGEBLOCK = document.getElementById('message-block');
+const SUBMITBUTTON = document.getElementById('submit-button');
+const CLOSEBUTTON = document.getElementById('close-button');
+const FORM = document.getElementById('form');
 
 NAV.addEventListener('click', event => {
   navLisA.forEach(a => a.classList.remove('active-menu'));
@@ -14,7 +18,6 @@ NAV.addEventListener('click', event => {
 });
 
 PICTUREGRID.addEventListener('click', event => {
-  // console.dir(event.target);
   portfolioPictures.forEach(img => img.classList.remove('active-picture'));
   if (event.target.tagName === 'IMG')
     event.target.classList.add('active-picture');
@@ -43,4 +46,23 @@ FILTER.addEventListener('click', ({ target, currentTarget }) => {
   console.log(target.closest('button'));
   filterButtons.forEach(({ classList }) => classList.remove('active-filter'));
   target.closest('button').classList.add('active-filter');
+});
+
+FORM.addEventListener('submit', event => {
+  const subject = document.getElementById('subject').value;
+  const details = document.getElementById('textarea').value;
+
+  const subjectSpan = document.getElementById('subject-span');
+  const descriptionSpan = document.getElementById('description-span');
+  subjectSpan.innerText = subject ? `Тема: ${subject}` : 'Без темы';
+
+  descriptionSpan.innerText = details ? `Описание: ${details}` : 'Без описания';
+  MESSAGEBLOCK.classList.remove('hidden');
+
+  event.preventDefault();
+});
+
+CLOSEBUTTON.addEventListener('click', () => {
+  MESSAGEBLOCK.classList.add('hidden');
+  FORM.reset();
 });
